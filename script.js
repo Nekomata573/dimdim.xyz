757a000c59cc13e3978543400fcf6e94f59800e7
Nekomata573 <139598219+Nekomata573@users.noreply.github.com>
2024-12-24 02:28:42 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Navigasi
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+});
+
+// Efek scroll halus
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
